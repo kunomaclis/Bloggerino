99c7c82f528b9a50ba24db9adcb9f1a829d9ae54
[Mitchell Hendee] <mitchellhendee@gmail.com>
2017-08-06 20:50:44 -0500

    okay I'm tapping out this is a mess

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 
 class PostsController < ApplicationController
   before_action :find_post, only: %i[edit update show delete]
-
+  before_action :authenticate_admin!, except: [:index, :show]
   # Index action to render all posts
   def index
     @posts = Post.all
