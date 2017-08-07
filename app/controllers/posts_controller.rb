07fd7cf242845b75931064c6561f4ead1e0549ba
[Mitchell Hendee] <mitchellhendee@gmail.com>
2017-08-06 20:04:55 -0500

    committing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,1 @@
-class PostsController < ApplicationController
-  # get a post
-  def index
-    @posts = Post.all
-  end
 
-  def show
-    @post = Post.find(params[:id])
-  end
-
-  # post a post
-  def create; end
-end
