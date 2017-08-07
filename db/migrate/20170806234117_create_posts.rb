00c87cb8f0d5f7c43b3e74e50da77ecc6ef377e3
[Mitchell Hendee] <mitchellhendee@gmail.com>
2017-08-06 19:25:27 -0500

    add some more changes to models

--- db/migrate/20170806234117_create_posts.rb
+++ db/migrate/20170806234117_create_posts.rb
@@ -3,7 +3,8 @@ class CreatePosts < ActiveRecord::Migration[5.1]
     create_table :posts do |t|
       t.string :title
       t.string :body
-
+      t.integer :user_id
+      
       t.timestamps
     end
   end
